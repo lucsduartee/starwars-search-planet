ec4b65eeb8486b3c2243663da9f17e035f600a90
Lucas Duarte <lucs.rkt@gmail.com>
2021-11-06 15:57:03 -0300

    Updates

---
+++ precache-manifest.bc4182860bf8c39b50b0a103071116f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ae7e249b70beaf09f79833abf651da",
+    "url": "/starwars-search-planet/index.html"
+  },
+  {
+    "revision": "5b965a0db5529e11edb3",
+    "url": "/starwars-search-planet/static/css/main.c8e09488.chunk.css"
+  },
+  {
+    "revision": "a7a3528ba07b8bfa19b7",
+    "url": "/starwars-search-planet/static/js/2.d0c10864.chunk.js"
+  },
+  {
+    "revision": "4662df181aea1747ef33698fb99e7915",
+    "url": "/starwars-search-planet/static/js/2.d0c10864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b965a0db5529e11edb3",
+    "url": "/starwars-search-planet/static/js/main.9c6d22d0.chunk.js"
+  },
+  {
+    "revision": "dc867879b3806996f3f3",
+    "url": "/starwars-search-planet/static/js/runtime-main.e67fed66.js"
+  },
+  {
+    "revision": "5a30b39d529a92eb389e0805879c11ca",
+    "url": "/starwars-search-planet/static/media/bbyodadef.5a30b39d.png"
+  }
+]);
